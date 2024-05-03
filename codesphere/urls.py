d77a0b034311175c30c967f486a46a1f89c7f427
ilyasolgun11 <127804888+ilyasolgun11@users.noreply.github.com>
2024-05-03 17:25:18 +0100

    Add static folders

--- codesphere/urls.py
+++ codesphere/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("accounts/", include("allauth.urls")),
+    path('summernote/', include('django_summernote.urls')),
     path('', include('base.urls'), name="base-urls"),
 ]
+
+# credit https://stackoverflow.com/questions/5517950/django-media-url-and-media-root
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
